1e37bf4359af6fb83ab40e96edec37791a550da4
Paul Airuehia <osasuair@gmail.com>
2024-03-24 16:24:54 -0400

    Add PostgreSQL properties file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,35 @@
 package org.example;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Objects;
+import java.util.Properties;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        String rootPath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("")).getPath();
+        String postgresPropPath = rootPath + "postgres.properties";
+
+        Properties postgresProps = new Properties();
+        postgresProps.load(new FileInputStream(postgresPropPath));
+
+        String user = postgresProps.getProperty("user");
+        String password = postgresProps.getProperty("password");
+
+        System.out.println("User: " + user);
+        System.out.println("Password: " + password);
     }
+
+
 
     static Connection connect(){
         String url = "jdbc:postgresql://localhost:5432/FinalProject";
         String user = "postgres";
-        String password = "postgres";
+        String password = "root";
         Connection connection = null;
         try{
             Class.forName("org.postgresql.Driver");
