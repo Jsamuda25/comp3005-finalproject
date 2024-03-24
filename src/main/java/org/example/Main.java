cdcf385d1eadfd22fa4e318635d8cadd62425750
Paul Airuehia <osasuair@gmail.com>
2024-03-24 15:56:17 -0400

    Add PostgreSQL Connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+    }
+
+    static Connection connect(){
+        String url = "jdbc:postgresql://localhost:5432/Assignment3";
+        String user = "postgres";
+        String password = "postgres";
+        Connection connection = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        }
+        catch(Exception e){
+            System.out.println("Unsuccessful connection to the PostgreSQL server.");
+        }
+        return connection;
     }
 }
