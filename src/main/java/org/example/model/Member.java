5e7956879e75aae0224ea43f3be6f8b569a791f4
Jsamuda25 <jsamuda25@gmail.com>
2024-03-28 22:50:33 -0400

    Functions for updating fitness goals - Member.java

--- src/main/java/org/example/model/Member.java
+++ src/main/java/org/example/model/Member.java
@@ -3,9 +3,11 @@ package org.example.model;
 import org.example.InputScanner;
 import org.example.PostgresConnection;
 
+import java.util.*;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
@@ -20,6 +22,7 @@ public class Member extends User{
         System.out.println("1. Profile information");
         System.out.println("2. Health metrics");
         System.out.println("3. Fitness goals");
+        System.out.println("4. Exit");
         System.out.print("Enter your choice as a number: ");
         int response = scanner.nextInt();
 
@@ -100,6 +103,7 @@ public class Member extends User{
                             System.out.println("Your new username is: " + username + "\n");
                             setUserName(username);
                         }
+
                     } catch (Exception e) {
                         System.out.println(e);
                         return false;
@@ -193,9 +197,197 @@ public class Member extends User{
 
     //helper function for profileManagement()
     public void updateFitnessGoals(){
-        System.out.println("Update Fitness Goals");
+        viewFitnessGoals();
+        Scanner scanner = InputScanner.getInstance();
+        System.out.print("Provide the ID of the goal you would like to edit, otherwise input 999: ");
+        int goal_id = scanner.nextInt();
+        scanner.nextLine();
+
+        if(goal_id == 999){
+            return;
+        }
+
+        System.out.println("What attributes would you like to change?");
+        System.out.println("1. Title");
+        System.out.println("2. Description");
+        System.out.println("3. End date");
+        System.out.println("4. Status");
+        System.out.println("5. Exit menu");
+        System.out.print("Enter choice as integer: ");
+        int response = scanner.nextInt();
+        scanner.nextLine();
+
+        if(response == 1){
+            System.out.print("Enter new title: ");
+            String title = scanner.nextLine();
+            updateTitle(goal_id, title);
+            updateFitnessGoals();
+        }
+        else if(response == 2){
+            System.out.print("Enter new description: ");
+            String des = scanner.nextLine();
+            updateDescription(goal_id, des);
+            updateFitnessGoals();
+        }
+        else if(response ==3){
+            System.out.print("Enter new end date (YYYY-MM-DD):  ");
+            String date =  scanner.nextLine();
+            modifyDate(goal_id, date);
+            updateFitnessGoals();
+        }
+        else if(response == 4){
+            System.out.println("Enter new status (0 = Incomplete, 1 = Complete): ");
+            int status = scanner.nextInt();
+            updateStatus(goal_id, status);
+            updateFitnessGoals();
+        }
+        else if (response==5){
+            profileManagement();
+        }
+        else{
+            return;
+        }
     }
 
+    public void updateTitle(int id, String title){
+        connection = PostgresConnection.connect();
+
+        try{
+            String query = "UPDATE fitnessgoal SET title = ? WHERE goalid = ? AND userid = ?";
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, title);
+            statement.setInt(2, id);
+            statement.setInt(3, getUserID());
+
+            int result = statement.executeUpdate();
+
+            if(result > 0){
+                try(ResultSet gKeys = statement.getGeneratedKeys()){
+                    if(gKeys.next()){
+                        System.out.println("Your goal has been updated.");
+                    }
+                }
+            }
+            else{
+                System.out.println("Goal was not updated.\n");
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void updateDescription(int id, String description){
+        connection = PostgresConnection.connect();
+
+        try{
+            String query = "UPDATE fitnessgoal SET value = ? WHERE goalid = ? AND userid = ?";
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, description);
+            statement.setInt(2, id);
+            statement.setInt(3, getUserID());
+
+            int result = statement.executeUpdate();
+
+            if(result > 0){
+                try(ResultSet gKeys = statement.getGeneratedKeys()){
+                    if(gKeys.next()){
+                        System.out.println("Your goal has been updated.");
+                    }
+                }
+            }
+            else{
+                System.out.println("Goal was not updated.\n");
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+    }
+
+    public void modifyDate(int id, String date){
+        connection = PostgresConnection.connect();
+
+        try{
+            String query = "UPDATE fitnessgoal SET enddate = ? WHERE goalid = ? AND userid = ?";
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setDate(1, java.sql.Date.valueOf(date));
+            statement.setInt(2, id);
+            statement.setInt(3, getUserID());
+
+            int result = statement.executeUpdate();
+
+            if(result > 0){
+                try(ResultSet gKeys = statement.getGeneratedKeys()){
+                    if(gKeys.next()){
+                        System.out.println("Your goal has been updated.");
+                    }
+                }
+            }
+            else{
+                System.out.println("Goal was not updated.\n");
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void updateStatus(int id, int status){
+        if (status < 0  || status > 1){
+            System.out.println("Invalid status value.");
+            return;
+        }
+
+        try{
+            String query = "UPDATE fitnessgoal SET status = ? WHERE goalid = ? AND userid = ?";
+            PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            statement.setInt(1, status);
+            statement.setInt(2, id);
+            statement.setInt(3, getUserID());
+
+            int result = statement.executeUpdate();
+
+            if(result > 0){
+                try(ResultSet gKeys = statement.getGeneratedKeys()){
+                    if(gKeys.next()){
+                        System.out.println("Your goal has been updated.");
+                    }
+                }
+            }
+            else{
+                System.out.println("Goal was not updated.\n");
+            }
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+
+    public void viewFitnessGoals(){
+        connection = PostgresConnection.connect();
+        System.out.println("Your fitness goals: ");
+        try{
+            String query = "SELECT goalid, title, value, enddate, status FROM fitnessgoal WHERE userid = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, getUserID());
+            ResultSet res = statement.executeQuery();
+            while(res.next()){
+                System.out.println("---------");
+                System.out.println("Goal id: " + res.getString("goalid"));
+                System.out.println("Title: " + res.getString("title"));
+                System.out.println("Description: " + res.getString("value"));
+                System.out.println("End date: " + res.getDate("enddate"));
+                System.out.println("Completion status: " + res.getInt("status") + "\n");
+            }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
     public void displayInfo(){
 
     }
